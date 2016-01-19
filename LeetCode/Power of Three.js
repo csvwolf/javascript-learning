8779e26666de9dbba20939504246eab8d0443f06
SkyAo <csvwolf@qq.com>
2016-01-19 23:27:05 +0800

    Solve Power Of Three -- LeetCode

---
+++ LeetCode/Power of Three.js
@@ -0,0 +1,44 @@
+/**
+ * Created by SkyAo on 16/1/19.
+ *
+ * Title: Power of Three
+ *
+ * Function: Given an integer, write a function to determine if it is a power of three.
+ *
+ * Number Of Methods Now: 2
+ *
+ * Problem Link: https://leetcode.com/problems/power-of-three/
+ */
+
+var isPowerOfThree = function(n) {
+    // 3^20 < 2^32 in the scope of integer
+     return (n > 0 && 3486784401 % n === 0);
+};
+
+var isPowerOfThree2 = function(n) {
+    // Common Method: Recursive
+
+    if (n <= 0) {
+        return false;
+    }
+
+    // 3^0
+
+    if (n == 1) {
+        return true;
+    }
+
+    if (n <= 3) {
+        return n % 3 === 0;
+    }
+
+    return isPowerOfThree(n / 3);
+};
+
+var isPowerOfThreeNotWork = function(n) {
+    // The Method works in Java, but not work in JavaScript
+    // If log3(n) is integer, the result is true. (Use Math method)
+     return (Math.log(n) / Math.log(3)) % 1 === 0;
+};
+
+console.log(isPowerOfThree(28));
