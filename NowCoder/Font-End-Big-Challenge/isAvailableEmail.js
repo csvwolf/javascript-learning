5b7f0e5bd7c13b3c9a596eb917c644e749ac9e91
SkyAo <csvwolf@qq.com>
2016-01-25 14:30:30 +0800

    solve isAvailableEmail

---
+++ NowCoder/Font-End-Big-Challenge/isAvailableEmail.js
@@ -0,0 +1,20 @@
+/**
+ * Created by SkyAo on 16/1/25.
+ *
+ * Title: 邮箱字符串判断
+ *
+ * Function: 判断输入是否是正确的邮箱格式
+ *
+ * Numbers Of Method: 1
+ */
+
+/**
+ * @param {String} sEmail
+ * @returns {boolean}
+ */
+var isAvailableEmail = function(sEmail) {
+    // {1,3}不能有空格,需要匹配多级域名
+    return /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9_\.\-]+(\.[a-zA-Z0-9_\-]+){1,4}$/.test(sEmail);
+};
+
+console.log(isAvailableEmail('skyao123@qq.com'));
