ff2f1e99fb7b574ea178a3f3859ca55b0142e877
SkyAo <csvwolf@qq.com>
2016-03-04 10:47:22 +0800

    Solved Balanced Binary Tree

---
+++ LeetCode/Easy/Balanced Binary Tree.js
@@ -0,0 +1,58 @@
+/**
+ * Created by SkyAo on 16/2/7.
+ *
+ * Title: Balanced Binary Tree
+ *
+ * Function: Given a binary tree, determine if it is height-balanced.
+ *
+ * Numbers Of Method:
+ *
+ * Problem Link: https://leetcode.com/problems/balanced-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Caculate Depth of the tree
+ * @param {TreeNode} root
+ * @return {int} depth
+ */
+var getDepth = function(root) {
+    if (!root)
+        return 0;
+    return 1 + Math.max(getDepth(root.left), getDepth(root.right));
+};
+
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    var depthLeft, depthRight;
+
+    if (!root)
+        return true;
+    if (!isBalanced(root.left)) {
+        return false;
+    }
+
+    if (!isBalanced(root.right)) {
+        return false;
+    }
+
+    depthLeft = getDepth(root.left);
+    depthRight = getDepth(root.right);
+
+    if (Math.abs(depthLeft - depthRight) > 1) {
+        return false;
+    }
+
+    return true;
+};
